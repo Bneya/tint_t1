ddeee4d87d4e614c39b75dd302b959f6f641eeb9
Benja <birivera@uc.cl>
2021-04-06 17:19:14 -0400

    Added season collapsibles

--- routes/seasons.js
+++ routes/seasons.js
@@ -1,22 +1,49 @@
 var express = require('express');
-// const axios = require('axios');
 
-// const axiosInstance = axios.create({
-//   baseURL: 'https://tarea-1-breaking-bad.herokuapp.com/api'
-// })
 
 const router = express.Router()
+
+// Funciones de utilidad
+function orderSerieBySeasons(serieObj) {
+  const orderedObj = {};
+
+  serieObj.forEach((ep) => {
+    // Crea la entrada si no existe
+    if (orderedObj[ep.season] === undefined) {
+      orderedObj[ep.season] = [];
+    }
+    // Añádelo a la lista
+    orderedObj[ep.season].push(ep);
+  });
+
+  const returnObjList = [];
+  Object.keys(orderedObj).forEach((season_num) => {
+    const season_eps = orderedObj[season_num]
+    const subObj = {
+      season_num,
+      season_eps
+    }
+    returnObjList.push(subObj);
+  });
+
+
+  console.log('orderedObj', returnObjList);
+  return returnObjList
+
+}
 
 // Todas las rutas de esta categoría
 router.get(`/`, async function (req, res) {
   // res.send('Hello seasons!');
   const ep_bcs = await req.axiosInstance.get('/episodes?series=Better+Call+Saul');
   const ep_bb = await req.axiosInstance.get('/episodes?series=Breaking+Bad');
+
+  const seasons_bb = orderSerieBySeasons(ep_bb);
   // console.log('episodes', episodes);
   // console.log("cosa prueba", req.prueba);
   res.render('seasons/index.ejs', {
     ep_bcs,
-    ep_bb
+    seasons_bb,
   });
 })
 
