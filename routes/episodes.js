bd587980982e9f90b1322f3af7285767bfd53594
Benja <birivera@uc.cl>
2021-04-06 17:54:59 -0400

    Added correct rendering in routing

--- routes/episodes.js
+++ routes/episodes.js
@@ -7,10 +7,17 @@ router.get(`/`, function (req, res) {
   res.send('Hello episodes!');
 })
 
-router.get(`/details`, function (req, res) {
+router.get(`/details`, async function (req, res) {
   console.log('req.params', req.query);
   const ep_id = req.query.id;
-  res.send(`Detalles del episodio ${ep_id}`);
+
+  // Obtiene detalles del episodio
+  const ep_details = await req.axiosInstance.get(`/episodes/${ep_id}`);
+  console.log('ep_details', ep_details);
+
+  res.render('episodes/details.ejs', {
+    episode: ep_details[0], 
+  });
 })
 
 module.exports = router;
