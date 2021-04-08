247d68bba235d65196bd4356de975933254e750d
Benja <birivera@uc.cl>
2021-04-08 18:06:18 -0400

    Character search working

--- routes/characters.js
+++ routes/characters.js
@@ -2,14 +2,43 @@ var express = require('express');
 
 const router = express.Router()
 
+// Funciones de utilidad
+function makeSearchable(charArray){
+  const searchableArray = [];
+  const spaceyRegex = /\s+/g;
+  charArray.forEach((char) => {
+    // const searchableChar = {
+    //   name: char,
+    //   searchableName: char.replace(spaceyRegex, "+")
+    // };
+    // searchableArray.push(searchableChar)
+    char.searchableName = char.name.replace(spaceyRegex, "+")
+  });
+
+  return charArray;
+}
+
 // Todas las rutas de esta categoría
 router.get(`/`, function (req, res) {
   res.send('Hello characters!');
 })
 
-router.post(`/search`, function (req, res) {
-  console.log("estamos en charsearch");
-  res.redirect("/characters/details?name=Saul+Goodman")
+router.post(`/search`, async function (req, res) {
+
+  // Busca en la API todos los resultados pertinentes y procésalos
+  const name = req.body.name;
+  const spaceyRegex = /\s+/g;
+  const searchableName = name.trim().replace(spaceyRegex, "+");
+  let char_list = await req.axiosInstance.get(`/characters?name=${searchableName}`);
+
+  char_list = makeSearchable(char_list);
+
+  console.log('char_list', char_list);
+  console.log('char_num', char_list.length);
+  res.render("characters/search", {
+    char_list,
+    char_num: char_list.length,
+  });
 })
 
 router.get(`/details`, async function (req, res) {
